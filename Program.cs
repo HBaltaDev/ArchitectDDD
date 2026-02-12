52967a9695d7033915945f7ec0966a34ad55c634
Hüseyin BALTA <huso_kado@hotmail.com>
2026-02-12 20:21:17 +0300

    refactor

--- Program.cs
+++ Program.cs
@@ -2,14 +2,9 @@
 
 using Microsoft.Extensions.Hosting;
 using NetStructre;
-
-// var builder = Host.CreateApplicationBuilder(args);
-
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddApplicationServices();
 builder.Services.AddHostedService<Worker>();
-
-//builder.Build().Run();
 
 await builder.Build().RunAsync();
